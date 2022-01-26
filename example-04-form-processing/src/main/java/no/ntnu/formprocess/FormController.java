2eddfdd584a6d8d243da4126864f46b9881cd595
Girts <girts.strazdins@gmail.com>
2022-01-26 23:10:08 +0100

    Simplify form processing example - no TymeLeaf, just plain JSON

--- example-04-form-processing/src/main/java/no/ntnu/formprocess/FormController.java
+++ example-04-form-processing/src/main/java/no/ntnu/formprocess/FormController.java
@@ -1,37 +1,21 @@
 package no.ntnu.formprocess;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FormController {
 
     /**
-     * This URL will show an HTML form with the post option. The form itself is placed within the Thymeleaf template,
-     * see /src/main/resources/templates
-     *
-     * @return Name of the Thymeleaf template to render
-     */
-    @GetMapping("/")
-    public String showForm() {
-        return "showForm";
-    }
-
-    /**
-     * This endpoint parses the HTTP POST request when the form is submitted - we simply collect the data and pass it
-     * to the Thymeleaf HTML  template which will show it.
-     * see /src/main/resources/templates
+     * This endpoint parses the HTTP POST request when the form is submitted
+     * - we simply collect the data and form a string with it in the response
      *
      * @param feedback Form data, injected (collected) in a Feedback object
-     * @param model    the data model to pass to Thymeleaf template engine
-     * @return Name of the Thymeleaf template to render
+     * @return The string to be returned to the client
      */
     @PostMapping("/hello")
-    public String handlePost(@ModelAttribute Feedback feedback, Model model) {
-        model.addAttribute("feedback", feedback);
-        return "showFormResults";
+    public Feedback handlePost(@ModelAttribute Feedback feedback) {
+        return feedback;
     }
 }
