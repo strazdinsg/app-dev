337dbe9bf2c1c66e1fb5598adc2f9e12fecd5e85
Girts <girts.strazdins@gmail.com>
2022-02-22 19:53:56 +0100

    Read also env vars from application.properties in example 09
    Update Readme

--- example-09-spring-env/README.md
+++ example-09-spring-env/README.md
@@ -3,4 +3,4 @@
 This project demonstrates how we can inject an environment variable into a field of a Spring Boot application. The
 respective class must be annotated with any @Component annotation, such as @RestController, @Service, etc.
 
-To see the value of your JAVA_HOME variable, access http://localhost:8080 in your browser (or Postman). 
+To see the value of some environment variables, access http://localhost:8080 in your browser (or Postman). 
--- example-09-spring-env/src/main/java/no/ntnu/example09springenv/HelloController.java
+++ example-09-spring-env/src/main/java/no/ntnu/example09springenv/HelloController.java
@@ -4,18 +4,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
     /** This variable will contain the value of environment variable JAVA_HOME - Spring boot will inject it */
     @Value("${JAVA_HOME}")
     private String javaPath;
+    @Value("${MY_ENV_VARIABLE}")
+    private String myCustomVariable;
+    @Value("${MY_INT_PARAM}")
+    private Integer myIntParam;
+
 
     /**
-     * Respond to HTTP GET / request with the value of JAVA_HOME environment variable
-     * @return Value of JAVA_HOME environment variable
+     * Respond to HTTP GET / request with values of some environment variables
+     * @return A Map (will be converted to JSON object) containing the variables
      */
     @GetMapping
-    public String getJavaPathVar() {
-        return javaPath;
+    public Map<String, Object> getEnvVariables() {
+        Map<String, Object> env = new HashMap<>();
+        env.put("JAVA_HOME", javaPath);
+        env.put("MY_ENV_VARIABLE", myCustomVariable);
+        env.put("my integer parameter", myIntParam);
+        return env;
     }
 }
