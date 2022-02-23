0d7a52342cf161d8367c8bc031108ffeab251152
Girts <girts.strazdins@gmail.com>
2022-02-23 01:01:42 +0100

    Add some simple database tests

--- example-11-spring-profiles/src/main/java/no/ntnu/Genre.java
+++ example-11-spring-profiles/src/main/java/no/ntnu/Genre.java
@@ -14,7 +14,8 @@ public class Genre {
     private Integer id;
     private String name;
 
-    public Genre() {}
+    public Genre() {
+    }
 
     public Genre(String name) {
         this.name = name;
@@ -34,5 +35,37 @@ public class Genre {
 
     public void setName(String genre) {
         this.name = genre;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Genre g2 && isIdEqualTo(g2.id) && isNameEqualTo(g2.name);
+    }
+
+    /**
+     * Check if provided name is equal to name of this object. Null-safe.
+     * @param name Name to compare
+     * @return True when names are equal
+     */
+    private boolean isNameEqualTo(String name) {
+        if (this.name != null) {
+            return this.name.equals(name);
+        } else {
+            return name == null;
+        }
+    }
+
+    /**
+     * Check if provided id is equal to ID of this object. Null-safe.
+     *
+     * @param id ID to compare
+     * @return True when the provided ID is equal to ID of this object
+     */
+    private boolean isIdEqualTo(Integer id) {
+        if (this.id != null) {
+            return this.id.equals(id);
+        } else {
+            return id == null;
+        }
     }
 }
