af2f0ab1232d9d6c9c50c30e41fda66b33798dfa
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-14 11:16:46 +0200

    Sign-up added

--- security-demos/06-monolith-session-auth/src/main/java/no/ntnu/controllers/HtmlPageController.java
+++ security-demos/06-monolith-session-auth/src/main/java/no/ntnu/controllers/HtmlPageController.java
@@ -1,12 +1,15 @@
 package no.ntnu.controllers;
 
+import no.ntnu.dto.SignupDto;
 import no.ntnu.services.AccessUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * A controller serving our HTML pages
@@ -55,5 +58,30 @@ public class HtmlPageController {
     @GetMapping("/login")
     public String loginForm() {
         return "login";
+    }
+
+    /**
+     * Sign-up form
+     * @return NAme of Thymeleaf template to use
+     */
+    @GetMapping("/signup")
+    public String signupForm() {
+        return "signup-form";
+    }
+
+    /**
+     * This method processes data received from the sign-up form (HTTP POST)
+     * @return NAme of the template for the result page
+     */
+    @PostMapping("/signup")
+    public String signupProcess(@ModelAttribute SignupDto signupData, Model model) {
+        model.addAttribute("signupData", signupData);
+        String errorMessage = userService.tryCreateNewUser(signupData.getUsername(), signupData.getPassword());
+        if (errorMessage == null) {
+            return "signup-success";
+        } else {
+            model.addAttribute("errorMessage", errorMessage);
+            return "signup-form";
+        }
     }
 }
