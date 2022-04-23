f7c531f9b400baef35e7640cd278c71c1c59b8a5
Girts <girts.strazdins@gmail.com>
2022-04-23 14:28:43 +0200

    Imitate a long operation for React backend example

--- example-14-backend-for-react/src/main/java/no/ntnu/reactbackend/controllers/ProductController.java
+++ example-14-backend-for-react/src/main/java/no/ntnu/reactbackend/controllers/ProductController.java
@@ -24,9 +24,12 @@ public class ProductController {
      * Get All products stored in the database
      *
      * @return All products in the storage
+     * @throws InterruptedException An exception if the executing thread is interrupted while it is sleeping
      */
     @GetMapping
-    public Collection<Product> getAll() {
+    public Collection<Product> getAll() throws InterruptedException {
+        // Imitate a long operation
+        Thread.sleep(2000);
         return productService.getAll();
     }
 }
