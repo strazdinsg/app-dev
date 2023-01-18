cf97397f2fa349f40ccbd3f480eb5d22bcd44063
Girts <girts.strazdins@gmail.com>
2023-01-18 12:26:06 +0100

    Code from class on 2023-01-18: Exercises 3.x

---
+++ live-coding-2023/week03/crud-demo/src/main/java/no/ntnu/CrudDemoApplication.java
@@ -0,0 +1,16 @@
+package no.ntnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main entrypoint for the Spring Boot application.
+ */
+@SpringBootApplication
+public class CrudDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrudDemoApplication.class, args);
+  }
+
+}
