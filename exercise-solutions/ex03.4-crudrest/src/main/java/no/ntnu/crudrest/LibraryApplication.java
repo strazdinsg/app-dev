1c418ed59ce262e4fbc42b10670b98de1572ec75
Girts Strazdins <girts.strazdins@gmail.com>
2023-01-31 20:42:24 +0100

    Rename the main class to LibraryApplication

--- exercise-solutions/ex03.4-crudrest/src/main/java/no/ntnu/crudrest/LibraryApplication.java
+++ exercise-solutions/ex03.4-crudrest/src/main/java/no/ntnu/crudrest/LibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudrestApplication {
+public class LibraryApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(CrudrestApplication.class, args);
+    SpringApplication.run(LibraryApplication.class, args);
   }
 
 }
