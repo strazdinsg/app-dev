c9f719ce8ed013e2c0a0b25fdd4dc1c3781ef7ec
Girts Strazdins <girts.strazdins@gmail.com>
2023-02-28 10:12:00 +0100

    Add "Learning web design"

--- exercises/full-stack-spring-boot/src/main/java/no/ntnu/tools/DummyDataInitializer.java
+++ exercises/full-stack-spring-boot/src/main/java/no/ntnu/tools/DummyDataInitializer.java
@@ -41,16 +41,19 @@ public class DummyDataInitializer implements ApplicationListener<ApplicationRead
     Author iSpector = new Author("Ian", "Spector", 1988);
     Author kRoss = new Author("Keith", "Ross", 1957);
     Author aKay = new Author("Adam", "Kay", 1980);
+    Author jRobbins = new Author("Jennifer", "Robbins", 1980);
 
     Genre gParody = new Genre("Parody");
     Genre gNetworks = new Genre("Computer Networks");
     Genre gPsychology = new Genre("Medical Applied Psychology");
     Genre gBio = new Genre("Autobiography");
+    Genre gWeb = new Genre("Web Development");
 
     genreRepository.save(gParody);
     genreRepository.save(gNetworks);
     genreRepository.save(gPsychology);
     genreRepository.save(gBio);
+    genreRepository.save(gWeb);
 
     Book bCompNet = new Book("Computer Networking", 2021, 800, gNetworks);
     Book b12Rules = new Book("12 Rules for Life", 2018, 409, gPsychology);
@@ -58,12 +61,14 @@ public class DummyDataInitializer implements ApplicationListener<ApplicationRead
         2007, 409, gParody);
     Book bBecoming = new Book("Becoming", 2018, 448, gBio);
     Book bHurt = new Book("This is Going to Hurt", 2018, 288, gBio);
+    Book bWeb = new Book("Learning Web Design", 2018, 808, gWeb);
 
     bookRepository.save(bCompNet);
     bookRepository.save(b12Rules);
     bookRepository.save(bChuck);
     bookRepository.save(bBecoming);
     bookRepository.save(bHurt);
+    bookRepository.save(bWeb);
 
     jKurose.addBook(bCompNet);
     kRoss.addBook(bCompNet);
@@ -71,6 +76,7 @@ public class DummyDataInitializer implements ApplicationListener<ApplicationRead
     iSpector.addBook(bChuck);
     mObama.addBook(bBecoming);
     aKay.addBook(bHurt);
+    jRobbins.addBook(bWeb);
 
     authorRepository.save(mObama);
     authorRepository.save(kRoss);
@@ -78,6 +84,7 @@ public class DummyDataInitializer implements ApplicationListener<ApplicationRead
     authorRepository.save(jKurose);
     authorRepository.save(iSpector);
     authorRepository.save(aKay);
+    authorRepository.save(jRobbins);
 
     logger.info("DONE importing test data");
   }
