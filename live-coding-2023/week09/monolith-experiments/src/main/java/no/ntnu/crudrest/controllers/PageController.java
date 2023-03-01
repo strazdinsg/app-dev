874e4649813bf792bac52b788ef851e1c334fb33
Girts <girts.strazdins@gmail.com>
2023-03-01 10:57:56 +0100

    Live coding: "About us" implemented, head extracted in a component

--- live-coding-2023/week09/monolith-experiments/src/main/java/no/ntnu/crudrest/controllers/PageController.java
+++ live-coding-2023/week09/monolith-experiments/src/main/java/no/ntnu/crudrest/controllers/PageController.java
@@ -1,6 +1,10 @@
 package no.ntnu.crudrest.controllers;
 
+import no.ntnu.crudrest.repositories.AuthorRepository;
+import no.ntnu.crudrest.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -8,12 +12,30 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class PageController {
+  @Autowired
+  BookRepository bookRepository;
+  @Autowired
+  AuthorRepository authorRepository;
+
   /**
    * Serve the "Home" page
+   *
    * @return Name of the ThymeLeaf template which will be used to render the HTML
    */
   @GetMapping("/")
   public String getHome() {
     return "index";
+  }
+
+  /**
+   * Serve the "About us" page
+   *
+   * @return Name of the ThymeLeaf template which will be used to render the HTML
+   */
+  @GetMapping("/about")
+  public String getAboutUs(Model model) {
+    model.addAttribute("bookCount", bookRepository.count());
+    model.addAttribute("authorCount", authorRepository.count());
+    return "about";
   }
 }
