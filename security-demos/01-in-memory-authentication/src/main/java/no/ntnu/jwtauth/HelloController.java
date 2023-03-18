df2196f80f14eb0f0555926bad9700040f33c3d3
Girts Strazdins <girts.strazdins@gmail.com>
2023-03-18 14:30:50 +0100

    Fix security example 01 - migrate to Spring Boot 3.0 (Spring Security 5)

--- security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/HelloController.java
+++ security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-  @GetMapping("")
+  @GetMapping("/")
   public String home() {
     return "This is accessible only to authorized users";
   }
--- security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/SecurityConfiguration.java
+++ security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/SecurityConfiguration.java
@@ -1,44 +1,60 @@
 package no.ntnu.jwtauth;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Creates AuthenticationManager - set up authentication type
  * The @EnableWebSecurity tells that this ia a class for configuring web security
  */
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfiguration {
   /**
    * This method will be called automatically by the framework to find out what authentication to use.
    * Here we specify static users and passwords that will be used for in-memory authentication
    *
-   * @param auth Authentication builder
+   * @param http HTTP Security configuration
    * @throws Exception
    */
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-        .withUser("chuck")
-        .password("$2a$12$/NoknpFFPDlzL3kBryJfsur0yeYC2JFqAs7Fd79ypMP6PN/mtSYmC") // == bcrypt("Nunchucks")
-        .roles("USER", "ADMIN")
-        .and()
-        .withUser("dave")
-        .password("$2a$10$nwbEjYKgcomq2rjUPge2JegqI.y4zEcNqRMPdqwFnd1ytorNCQM/y") // == bcrypt("user")
-        .roles("USER");
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        // This enables the access restrictions
+        .authorizeHttpRequests()
+        // This says that access to all URLs requires any authenticated user (no specific role)
+        .requestMatchers("/**").authenticated()
+        // Redirect to an auto-provided login-form if the user is not authenticated
+        .and().formLogin();
+
+    return http.build();
   }
 
   /**
-   * This method is called to decide what encryption to use for password checking
+   * This will be used as the main service for providing users for the security manager
    *
-   * @return The password encryptor
+   * @return A UserDetailsService managing all the allowed users, their passwords and roles
    */
   @Bean
-  public PasswordEncoder getPasswordEncoder() {
-    return new BCryptPasswordEncoder();
+  public UserDetailsService users() {
+    UserDetails user = User.builder()
+        .username("chuck")
+        // password=Nunchucks
+        .password("{bcrypt}$2a$12$/NoknpFFPDlzL3kBryJfsur0yeYC2JFqAs7Fd79ypMP6PN/mtSYmC")
+        .roles("USER", "ADMIN")
+        .build();
+    UserDetails admin = User.builder()
+        .username("dave")
+        // password=user
+        .password("{bcrypt}$2a$10$nwbEjYKgcomq2rjUPge2JegqI.y4zEcNqRMPdqwFnd1ytorNCQM/y")
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(user, admin);
   }
+
 }
