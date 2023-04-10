85dc86f29df17a5e88e4398471c1588fd8168532
Girts Strazdins <girts.strazdins@gmail.com>
2023-04-10 11:49:42 +0200

    Example 13 - reading .env file in Spring Boot

---
+++ example-13-env-files/README.md
@@ -0,0 +1,9 @@
+# Example of accessing .env files
+
+A project showing how you can store environment variables in an `.env` file and access those
+in your Spring Boot project.
+
+You can see the values by opening `http://localhost:8080` in the browser.
+
+Used in the
+course [IDATA2306 Application Development](https://www.ntnu.edu/studies/courses/IDATA2306)
---
+++ example-13-env-files/src/main/java/no/ntnu/HelloController.java
@@ -0,0 +1,47 @@
+package no.ntnu;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST API controller.
+ */
+@RestController
+public class HelloController {
+  /**
+   * This variable will contain the value of environment variable JAVA_HOME - Spring Boot will
+   * inject it.
+   */
+  @Value("${java.home}")
+  private String javaPath;
+  @Value("${MY_ENV_VARIABLE}")
+  private String myCustomVariable;
+  @Value("${MY_INT_PARAM}")
+  private Integer myIntParam;
+  /**
+   * This value will come from a command line parameter. To set it, run the application
+   * with --SERVER_FLAG=yourValue
+   * We use a hack in the application.properties to set default value for it.
+   */
+  @Value("${server.flag}")
+  private String serverFlag;
+
+
+  /**
+   * Respond to HTTP GET / request with values of some environment variables.
+   *
+   * @return A Map (will be converted to JSON object) containing the variables
+   */
+  @GetMapping("/")
+  public Map<String, Object> getEnvVariables() {
+    Map<String, Object> env = new HashMap<>();
+    env.put("JAVA_HOME", javaPath);
+    env.put("MY_ENV_VARIABLE", myCustomVariable);
+    env.put("my integer parameter", myIntParam);
+    env.put("server.flag", serverFlag);
+    return env;
+  }
+}
