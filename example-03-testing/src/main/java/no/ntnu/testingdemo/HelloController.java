ec06fc934cf5a0566dab7c065c4cd8eb3a5ce82d
Girts Strazdins <girts.strazdins@gmail.com>
2024-02-03 11:49:50 +0100

    Update to latest Spring Boot, CheckStyle compliance

--- example-03-testing/src/main/java/no/ntnu/testingdemo/HelloController.java
+++ example-03-testing/src/main/java/no/ntnu/testingdemo/HelloController.java
@@ -2,16 +2,20 @@ package no.ntnu.testingdemo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * REST API controller, with some dummy endpoints, for testing
+ * REST API controller, with some dummy endpoints, for testing.
  */
 @RestController
 public class HelloController {
 
   /**
-   * Return a simple message to HTTP GET /hello
+   * Return a simple message to HTTP GET /hello .
    *
    * @return A simple mess
    */
@@ -21,7 +25,7 @@ public class HelloController {
   }
 
   /**
-   * Return code 501 NOT IMPLEMENTED on HTTP DELETE /delete
+   * Return code 501 NOT IMPLEMENTED on HTTP DELETE /delete .
    *
    * @return Response with status code 501
    */
@@ -31,7 +35,7 @@ public class HelloController {
   }
 
   /**
-   * Return a Hello object, encoded as JSON string to HTTP GET /hello/object
+   * Return a Hello object, encoded as JSON string to HTTP GET /hello/object .
    *
    * @return Hello object (JSON)
    */
@@ -41,7 +45,7 @@ public class HelloController {
   }
 
   /**
-   * Simulate addition of a new object on HTTP POST /add
+   * Simulate addition of a new object on HTTP POST /add .
    *
    * @param hello The hello object to add, received from HTTP request body, JSON string
    * @return Status code 201 CREATED when hello is not null, 406 NOT ACCEPTABLE otherwise
--- example-03-testing/src/main/java/no/ntnu/testingdemo/TestingDemoApplication.java
+++ example-03-testing/src/main/java/no/ntnu/testingdemo/TestingDemoApplication.java
@@ -3,6 +3,9 @@ package no.ntnu.testingdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main Spring Boot application class.
+ */
 @SpringBootApplication
 public class TestingDemoApplication {
 
