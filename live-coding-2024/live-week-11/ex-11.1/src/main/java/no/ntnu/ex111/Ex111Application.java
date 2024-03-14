759987e10a335b9f80cbd01ea824972b2943b25b
Girts Strazdins <girts.strazdins@gmail.com>
2024-03-14 10:08:58 +0100

    Starter code for live coding of exercise 11.1

---
+++ live-coding-2024/live-week-11/ex-11.1/src/main/java/no/ntnu/ex111/Ex111Application.java
@@ -0,0 +1,13 @@
+package no.ntnu.ex111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex111Application.class, args);
+	}
+
+}
---
+++ live-coding-2024/live-week-11/ex-11.1/src/test/java/no/ntnu/ex111/Ex111ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.ex111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
