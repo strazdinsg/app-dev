2a1c4e79d7d19c4482c5a88c89fa16c744c495d0
Girts Strazdins <girts.strazdins@miles.no>
2024-03-14 07:14:12 +0100

    Update to the latest Spring Boot version

--- example-07-monolith/01-monolith/src/main/java/no/ntnu/repository/GenreRepository.java
+++ example-07-monolith/01-monolith/src/main/java/no/ntnu/repository/GenreRepository.java
@@ -4,7 +4,7 @@ import no.ntnu.model.Genre;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * Repository interface for accessing Gende data in the database.
+ * Repository interface for accessing Genre data in the database.
  * Spring will auto-generate necessary methods.
  */
 public interface GenreRepository extends CrudRepository<Genre, Integer> {
