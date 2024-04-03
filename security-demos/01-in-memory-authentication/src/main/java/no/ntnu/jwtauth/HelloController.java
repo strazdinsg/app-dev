492c762f0b98e21bccad4e74a27fb613fc3c27f6
Girts Strazdins <girts.strazdins@gmail.com>
2024-04-03 13:34:49 +0200

    Update to the latest Spring version, fix security config syntax.
    Also: CheckStyle cleanup

--- security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/HelloController.java
+++ security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Simple REST API controller
+ * Simple REST API controller.
  */
 @RestController
 public class HelloController {
--- security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/SecurityConfiguration.java
+++ security-demos/01-in-memory-authentication/src/main/java/no/ntnu/jwtauth/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package no.ntnu.jwtauth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,33 +11,46 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * Creates AuthenticationManager - set up authentication type
+ * Creates AuthenticationManager - set up authentication type.
  * The @EnableWebSecurity tells that this ia a class for configuring web security
  */
 @Configuration
 public class SecurityConfiguration {
   /**
-   * This method will be called automatically by the framework to find out what authentication to use.
+   * This method will be called automatically by the framework to find the authentication to use.
    * Here we specify static users and passwords that will be used for in-memory authentication
    *
    * @param http HTTP Security configuration
-   * @throws Exception
+   * @throws Exception On authentication error
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // All requests must be authenticated. Redirect to login-form, when not authenticated.
+    // Note: this works starting from Spring 6.1 (Spring Boot 3.2).
+
     http
-        // This enables the access restrictions
-        .authorizeHttpRequests()
-        // This says that access to all URLs requires any authenticated user (no specific role)
-        .requestMatchers("/**").authenticated()
-        // Redirect to an auto-provided login-form if the user is not authenticated
-        .and().formLogin();
+        // Access to all URLs requires any authenticated user (no specific role)
+        .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+        // Use standard login-form
+        .formLogin(Customizer.withDefaults());
 
     return http.build();
+
+    // In earlier Spring versions this worked instead:
+    //    http
+    //        // This enables the access restrictions
+    //        .authorizeHttpRequests()
+    //        // This says that access to all URLs requires any authenticated user
+    //        // (no specific role)
+    //        .requestMatchers("/**").authenticated()
+    //        // Redirect to an auto-provided login-form if the user is not authenticated
+    //        .and().formLogin();
+    //
+    //    return http.build();
   }
 
   /**
-   * This will be used as the main service for providing users for the security manager
+   * This will be used as the main service for providing users for the security manager.
    *
    * @return A UserDetailsService managing all the allowed users, their passwords and roles
    */
