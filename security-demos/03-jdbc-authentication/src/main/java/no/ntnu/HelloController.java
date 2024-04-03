8f0ee7120dcd607e5bfffd89b4a3b549d344f333
Girts Strazdins <girts.strazdins@gmail.com>
2024-04-03 14:24:14 +0200

    Update to the latest Spring version, fix security config syntax and style

--- security-demos/03-jdbc-authentication/src/main/java/no/ntnu/HelloController.java
+++ security-demos/03-jdbc-authentication/src/main/java/no/ntnu/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * A simple REST API controller providing different endpoints
+ * A simple REST API controller providing different endpoints.
  */
 @RestController
 public class HelloController {
--- security-demos/03-jdbc-authentication/src/main/java/no/ntnu/SecurityConfiguration.java
+++ security-demos/03-jdbc-authentication/src/main/java/no/ntnu/SecurityConfiguration.java
@@ -1,15 +1,16 @@
 package no.ntnu;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 /**
  * Creates AuthenticationManager - set up authentication type.
@@ -17,7 +18,8 @@ import javax.sql.DataSource;
 @Configuration
 public class SecurityConfiguration {
   /**
-   * A database connection which provides our users. We configure the database in application.properties
+   * A database connection which provides our users. We configure the database in
+   * application.properties
    */
   @Autowired
   DataSource dataSource;
@@ -46,25 +48,45 @@ public class SecurityConfiguration {
   }
 
   /**
-   * This method will be called automatically by the framework to find out what authentication to use.
+   * This method will be called automatically by the framework to find the authentication to use.
    *
    * @param http HttpSecurity setting builder
-   * @throws Exception
+   * @throws Exception On authentication error
    */
   @Bean
   public SecurityFilterChain configureAuthorizationFilterChain(HttpSecurity http) throws Exception {
-    // Set up the authorization requests, starting from most restrictive at the top, to least restrictive on bottom
+    // For Spring Boot 3.2.4 and newer, use this configuration
     http
-        // This enables the access restrictions
-        .authorizeHttpRequests()
-        // This configures the requested authorization (role)
-        .requestMatchers("/admin").hasRole("ADMIN")
-        .requestMatchers("/user").hasAnyRole("USER", "ADMIN")
-        .requestMatchers("/").permitAll()
-        // Redirect to an auto-provided login-form if the user is not authenticated
-        .and().formLogin();
-
+        // Disable CSRF and CORS checks. Without this it will be hard to make automated tests.
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        // This configures the requested authorization (role):
+        // All routes starting with /admin require ADMIN role
+        .authorizeHttpRequests((auth) -> auth.requestMatchers("/admin").hasRole("ADMIN"))
+        // All routes starting with /user require USER role
+        .authorizeHttpRequests((auth) ->
+            auth.requestMatchers("/user").hasAnyRole("ADMIN", "USER"))
+        // The default URL / is accessible to everyone
+        .authorizeHttpRequests((auth) -> auth.requestMatchers("/").permitAll())
+        .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+        // Use standard login-form
+        .formLogin(Customizer.withDefaults());
     return http.build();
+
+    // For Spring Boot 3.0.2 (and older?) use the following:
+    //    http
+    //        // Disable CSRF and CORS checks. Without this it will be hard to make automated tests.
+    //        .cors().and().csrf().disable()
+    //        // This enables the access restrictions
+    //        .authorizeHttpRequests()
+    //        // This configures the requested authorization (role)
+    //        .requestMatchers("/admin").hasRole("ADMIN")
+    //        .requestMatchers("/user").hasAnyRole("USER", "ADMIN")
+    //        .requestMatchers("/").permitAll()
+    //        // Redirect to an auto-provided login-form if the user is not authenticated
+    //        .and().formLogin();
+    //
+    //    return http.build();
   }
 
   /**
